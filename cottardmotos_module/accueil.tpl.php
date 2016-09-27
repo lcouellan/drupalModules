27b205ad9344dd628b1ec7ff6d3ae4696f758b80
Lénaïc <lcouellan@gmail.com>
2016-09-27 16:59:08 +0200

    First Commit

---
+++ cottardmotos_module/accueil.tpl.php
@@ -0,0 +1,22 @@
+<div class="row line1">
+	<div class="col-xs-6"><?php echo $element->cottardbike; ?></div>
+	<div class="col-xs-6"><?php echo $element->cottardmotos; ?></div>
+	<div class="col-xs-6"><?php echo $element->cottardoccasions; ?></div>
+	<div class="col-xs-6"><?php echo $element->maxxess; ?></div>
+</div>
+<div class="row line2">
+	<div class="col-xs-6"><?php echo $element->actualites; ?></div>
+	<div class="col-xs-6"><?php echo $element->galerie_photos; ?></div>
+</div>
+<div class="row line3">
+	<div class="col-xs-6"><?php echo $element->recherche_rapide; ?></div>
+	<div class="col-xs-6"><?php echo $element->nos_selections; ?></div>
+</div>
+<div class="row line4">
+	<div class="col-xs-4"><?php echo $element->services; ?></div>
+	<div class="col-xs-4"><?php echo $element->reprises ?></div>
+	<div class="col-xs-4"><?php echo $element->preparations ?></div>
+</div>
+<div class="row line5">
+	<div class="col-xs-12"><?php echo $element->pub; ?></div>
+</div>
